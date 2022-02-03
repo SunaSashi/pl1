5eb2be7ba53c0e2e8796bb8bcd7070e1336c4795
SunaSashi <sunayana.sashikumar@flemingcollege.ca>
2022-02-03 12:57:22 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,11 +1,16 @@
-// This example displays a map with the language and region set
-// to Japan. These settings are specified in the HTML script element
-// when loading the Google Maps JavaScript API.
-// Setting the language shows the map in the language of your choice.
-// Setting the region biases the geocoding results to that region.
+
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
-    center: { lat: 35.717, lng: 139.731 },
+    center: { lat: 10.166667, lng: 77.066667 },
+  });
+  const marker = new google.maps.Marker({
+    position: center,
+    map,
+    title: "Click to zoom",
+  });
+  marker.addListener("click", () => {
+    map.setZoom(8);
+    map.setCenter(marker.getPosition());
   });
 }
